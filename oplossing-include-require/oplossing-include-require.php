c3787d41ce1c312ce67d7ba6b29678dbc72b5afa
Sander Borret <sander.borret@student.kdg.be>
2016-10-27 18:12:20 +0200

    Oplossing include-require toegevoegd

---
+++ oplossing-include-require/oplossing-include-require.php
@@ -0,0 +1,25 @@
+<?php
+	$artikels = array(
+		array(
+			"title" => "PHP 8.4 gereleased",
+			"text" => "De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, 
+					in-browser 3d projecties en de API voor drones.",
+			"tags" => "new, PHP 8.4"
+			),
+		array(
+			"title" => "Zymphovel framework",
+			"text" => "Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?",
+			"tags" => "frameworks"
+			),
+		array(
+			"title" => "Rasmus Lerdorf vermist",
+			"text" => "De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet 
+			richting Ibiza vertrok.",
+			"tags" => "bizar, rasmus lerdorf, feestneus"
+			),
+		);
+
+	include "view/header-partial.html";
+	include "view/body-partial.html";
+	include "view/footer-partial.html";
+?>
