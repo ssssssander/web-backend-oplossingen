5e53bfa88f4b3327c1242dde7e203772ff3aa750
Sander Borret <sander.borret@student.kdg.be>
2016-11-10 23:02:43 +0100

    Oplossing functions deel 2 toegevoegd

--- oplossing-functions/oplossing-functions-deel2.php
+++ oplossing-functions/oplossing-functions-deel2.php
@@ -1,20 +1,50 @@
 <?php
-	$array = array("HP", "Acer", "Asus", "Sony", "Apple", "Microsoft");
-	function drukArrayAf($array) {
-		$output = "";
-		for($i = 0; $i < count($array); $i++) {
-			$output .= nl2br("merken[ " . $i . " ] heeft waarde '" . $array[$i] . "'\n");
-		}
-		return $output;
-	}
+	$flatArray = array("Jan", "Jaap", "Joris", "Jonas");
+	$multiArray = array(
+		"computerMerken" => array("HP", "Acer", "Asus", "Sony", "Apple", "Microsoft"),
+		"playstationGames" => array("God of War", "Uncharted", "Ratchet & Clank")
+	);
+	$html = "<html><head></head><body>AAAAAA</body></html>";
 
-	// Niet af
+	function drukArrayAf($array) {
+		$outputArray = array();
+
+		foreach($array as $innerArrayKey => $innerArray) {
+			if(is_array($innerArray)) {
+				foreach($innerArray as $valueKey => $value) {
+					array_push($outputArray, $innerArrayKey . "[ " . $valueKey . " ] heeft waarde '" . $value . "'");
+				}
+			}
+			else {
+				$valueKey = $innerArrayKey;
+				$value = $innerArray;
+				array_push($outputArray, "array[ " . $valueKey . " ] heeft waarde '" . $value . "'");
+			}
+		}
+		return $outputArray;
+	}
+	$flatArrayOutput = drukArrayAf($flatArray);
+	$multiArrayOutput = drukArrayAf($multiArray);
+
+	function validateHtmlTag($html) {
+		$openingTag = "<html>";
+		$closingTag = "</html>";
+
+		return ((strpos($html,$openingTag) === 0) && (strpos($html, $closingTag, strlen($html) - strlen($closingTag)) !== false));
+	}
+	$isValid = validateHtmlTag($html);
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head></head>
 	<body>
-		<p><?= drukArrayAf($array) ?></p>
+		<?php foreach($flatArrayOutput as $outputItem): ?>
+			<p><?= $outputItem ?></p>
+		<?php endforeach ?>
+		<?php foreach($multiArrayOutput as $outputItem): ?>
+			<p><?= $outputItem ?></p>
+		<?php endforeach ?>
+		<p><code><?= htmlentities($html) ?></code> is <?= $isValid ? "geldig" : "niet geldig" ?></p>
 	</body>
 </html>
