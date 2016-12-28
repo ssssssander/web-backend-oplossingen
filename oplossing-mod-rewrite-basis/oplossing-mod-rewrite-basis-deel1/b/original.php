8b1dcc59784355017f743941275d1297ea053151
Sander Borret <sander.borret@student.kdg.be>
2016-12-28 22:43:08 +0100

    Oplossing mod-rewrite-basis toegevoegd

---
+++ oplossing-mod-rewrite-basis/oplossing-mod-rewrite-basis-deel1/b/original.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<h1>Het originele bestand.</h1>
+	</body>
+</html>
---
+++ oplossing-mod-rewrite-basis/oplossing-mod-rewrite-basis-deel5/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<h1>var_dump() van $_GET variabele</h1>
+		<?= var_dump($_GET) ?>
+	</body>
+</html>
